6e880d4c6e79665505899c5b8fb8985964092cb9
Sajdeen Hussain <135839408+sajdeenhussain@users.noreply.github.com>
2023-09-18 00:43:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//3. Declare and initialize a strings array.
+// var arr = ["abc","def","ghi","jkl",'mno'];
+
+
+//Declare and initialize a numbers array.
+// var numbers = [1, 2, 3, 4, 5];
+// console.log(numbers)
+
+//Declare and initialize a boolean array.
+// var boo = [true, false];
+// console.log(boo)
+
+// //Declare and initialize a mixed array.
+// var mixed = ["abc", 2, true, "def"];
+// console.log(mixed)
+
+// //Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// document.write(
+//     "<h1>"+qualifications+"</h1>"
+
+// )
+
+// Declare an empty array using JS literal notation to store
+// student names in future.
+// var studentNames = [];
+
+
+// Declare an empty array using JS object notation to store
+// student names in future.
